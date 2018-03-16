6a0b9a01eca1ae84f1137af071a2c07b7c71c1f4
Panyushenko <macbookair@MacBook-Air.local>
2018-03-16 13:54:29 +0300

    viewMonipulationTree with full View Controller(add view Image; View: stepper, label, switch, slider)

---
+++ viewMonipulationTree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  viewMonipulationTree
+//
+//  Created by Panyushenko on 16.03.2018.
+//  Copyright © 2018 Artyom Panyushenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
